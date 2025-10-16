6d4e89f32d48e8ebaf383fdb53f48d13f97a09b0
kmw <skqjahjakskxl@gmail.com>
2025-10-16 15:26:35 +0900

    feat(parser): 입력 시작이 숫자인지, // 문자열인지 판별 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    const delimiter = [",", ";"];
 
+    if (+input[0] === NaN) {
+      // 커스텀구분자인지 확인
+    }
+    
     Console.print(`결과 : ${input}`);
   }
 }
