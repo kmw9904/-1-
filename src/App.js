5217f394970d5eb6fb28dc12b42c1b82f0c5b9c6
김명원 <skqjahjakskxl@gmail.com>
2025-10-19 19:39:57 +0900

    feat(sign-number-parser): 음수 부호 올 때 음수처리 기능 수정 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 import CustomDelimiter from "./CustomDelimiter.js";
 import SignNumberParser from "./SignNumberParser.js";
-import ConatinsDelimiter from "./SplitByDelimiter.js";
-import Sum from "./Sum.js";
 
 class App {
   async run() {
@@ -10,22 +8,14 @@ class App {
       "덧셈할 문자열을 입력해 주세요.\n"
     );
 
-    const { text, delimiter } = CustomDelimiter(input, [",", ";"]);
+    const { text, delimiters } = CustomDelimiter(input.replace(/\\n/g, "\n"), [
+      ",",
+      ";",
+    ]);
 
-    // 숫자가 음수인지 확인
-    SignNumberParser(text);
+    const { signChangeText } = SignNumberParser(text, delimiters);
 
-    // 구분자로 구성되어 있는지 확인
-    const isConatins = ConatinsDelimiter(text, delimiter);
-
-    if (!isConatins) {
-      throw Error("구분자 오류");
-    }
-
-    // 여태까지 오류가 없다면 더하기
-    const result = Sum(text);
-
-    Console.print(`결과 : ${result}`);
+    Console.print(`결과 : ${signChangeText}`);
   }
 }
 
--- src/SignNumberParser.js
+++ src/SignNumberParser.js
@@ -1,12 +1,47 @@
-export default function SignNumberParser(text) {
-  const numberParser = [];
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] === "-") {
-      numberParser.push(text[i] + text[i + 1]);
-      i++;
-    } else {
-      numberParser.push(text[i]);
+export default function SignNumberParser(text, delimiters) {
+  // 먼저 숫자는 문자열을 숫자로 변환하기
+
+  const ln = text.length;
+  const signChangeText = [];
+  if (delimiters.includes("-")) {
+    for (let i = 0; i < ln; i++) {
+      if (text[i] === "-" && text[i + 1] === "-" && +text[i + 2] >= 0) {
+        signChangeText.push(text[i]);
+
+        // 음수 부호로 만든 text
+        let minusText = text[i + 1] + text[i + 2];
+        let afterMinus = 0;
+        // 뒤에 더 숫자가 있는지?
+        while (i + 3 + afterMinus < ln && +text[i + 3 + afterMinus] >= 0) {
+          minusText += text[i + 3 + afterMinus];
+          afterMinus += 1;
+        }
+        signChangeText.push(minusText);
+        i += 2 + afterMinus;
+      } else {
+        signChangeText.push(text[i]);
+      }
+    }
+  } else {
+    for (let i = 0; i < ln; i++) {
+      if (text[i] === "-" && +text[i + 1] >= 0) {
+        // 음수 부호로 만든 text
+        let minusText = text[i] + text[i + 1];
+        let afterMinus = 0;
+        // 뒤에 더 숫자가 있는지?
+        while (i + 2 + afterMinus < ln && +text[i + 2 + afterMinus] >= 0) {
+          minusText += text[i + 2 + afterMinus];
+          afterMinus += 1;
+        }
+        signChangeText.push(minusText);
+        i += 1 + afterMinus;
+      } else {
+        signChangeText.push(text[i]);
+      }
     }
   }
-  return numberParser;
+
+  return { signChangeText };
 }
+
+console.log(SignNumberParser("1--21n3".split(""), [";", ",", "n", "-"]));
